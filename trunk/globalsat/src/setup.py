d805a4834c2bf268b07b00b7caa3ad18034c56fb
speigei <speigei@ac1b979b-6330-0410-a4b8-2fc45048b05a>
2007-06-10 14:46:29 +0000

    fixed gpxExportTemplate to generate valid xml

--- trunk/globalsat/src/setup.py
+++ trunk/globalsat/src/setup.py
@@ -19,7 +19,7 @@ setup(
 setup(
       console = ['..\\src\\gh615_console.py'],
       #data_files = [("exportTemplates",files),("export",[])],
-      data_files = [("",["..\\src\\config.xml"]),("exportTemplates",glob.glob("..\\src\\exportTemplates\\*.txt")),("export",[])],
+      data_files = [("",["..\\src\\config.ini"]),("exportTemplates",glob.glob("..\\src\\exportTemplates\\*.txt")),("export",[])],
       zipfile = None,
       description = "gh615 console",
       version = "0.1",
