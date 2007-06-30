9918eb1a579cabca31ebd31d8b60ff8ec9dfcfe1
speigei <speigei@ac1b979b-6330-0410-a4b8-2fc45048b05a>
2007-06-30 16:03:23 +0000

    cleaned up setup.py

--- trunk/globalsat/src/setup.py
+++ trunk/globalsat/src/setup.py
@@ -2,20 +2,6 @@ from distutils.core import setup
 import py2exe
 import sys, os, glob
 
-'''
-setup(
-     name = "gh615_console",
-     console = ['..\\src\\gh615_console.py'],
-     data_files = [("exportTemplates",files),("export",[]),("","..\\src\\config.xml")],
-     zipfile = "stuff.zip",
-     description = "gh615 console",
-     version = "0.1",
-     author = "Till Hennig",
-     #author_email = "cliechti@gmx.net",
-     url = "http://code.google.com/p/gh615/"
-)
-'''
-
 setup(
       console = ['..\\src\\gh615_console.py'],
       #data_files = [("exportTemplates",files),("export",[])],
